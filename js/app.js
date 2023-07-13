9bcd3ea965641efc0e6bcb4341ca0888dc9414c6
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-07-13 14:48:48 +0100

    updated branch

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
-let rows = prompt('How many rows would you like in your pyramid?');
-for (let i = 1; i <= rows; i++) {
-  let str = ''.repeat(rows-i);
-  str += '*'.repeat((2 * i) - 1);
-  console.log(str);
-}
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
